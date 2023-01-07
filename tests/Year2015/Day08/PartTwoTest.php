aa3cb8e32ba2e84089d4272d75480c299dff2bec
Gunter Solf <gunter.solf@gmail.com>
2023-01-07 15:21:10 +0100

    2015 08 solution

---
+++ tests/Year2015/Day08/PartTwoTest.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+
+namespace AOCTest\Year2015\Day08;
+
+use AOC\Year2015\Day08\PartTwo;
+use AOC\Test\Util\SolutionTest;
+
+class PartTwoTest extends SolutionTest
+{
+    /** @var class-string */
+    public string $solutionClass = PartTwo::class;
+
+    /**
+     * @return array<int, array<mixed>>
+     */
+    public function data(): array
+    {
+        return [
+            [['""'], 4],
+            [['"abc"'], 4],
+            [['"aaa\"aaa"'], 6],
+            [['"\x27"'], 5],
+            [['input.txt'], 2046],
+        ];
+    }
+}
