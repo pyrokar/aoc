be432b8b7fcf2cc3c8f8976dc062bbdfed700bec
Gunter Solf <gunter.solf@gmail.com>
2023-11-30 22:41:54 +0100

    2020 07 solution

---
+++ src/Year2020/Day07/PartOne.php
@@ -0,0 +1,72 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AOC\Year2020\Day07;
+
+use Generator;
+
+use Safe\Exceptions\PcreException;
+
+use function Safe\preg_match;
+
+class PartOne
+{
+    /**
+     * @param Generator<string> $input
+     *
+     * @throws PcreException
+     *
+     * @return int
+     */
+    public function __invoke(Generator $input): int
+    {
+        $bags = [];
+
+        foreach ($input as $line) {
+            if (!preg_match('/(?<color>\w+ \w+) bags contain (?<contain>[^.]+)\./', $line, $m)) {
+                continue;
+            }
+
+            $color = $m['color'];
+            $containedBags = explode(', ', $m['contain']);
+
+            foreach ($containedBags as $bag) {
+                if (!preg_match('/\d+ (?<color>\w+ \w+) bags?/', $bag, $m)) {
+                    continue;
+                }
+
+                $containedColor = (string) $m['color'];
+
+                if (!isset($bags[$containedColor])) {
+                    $bags[$containedColor] = [];
+                }
+
+                $bags[$containedColor][] = $color;
+            }
+        }
+
+        $this->countBags('shiny gold', $bags, $containingBags);
+
+        return count($containingBags);
+    }
+
+    /**
+     * @param string $color
+     * @param array<string, list<string>> $bags
+     * @param array<string, 1>|null $containingBags
+     *
+     * @return void
+     */
+    private function countBags(string $color, array $bags, ?array &$containingBags = []): void
+    {
+        if (!isset($bags[$color])) {
+            return;
+        }
+
+        foreach ($bags[$color] as $containingColor) {
+            $containingBags[$containingColor] = 1;
+            $this->countBags($containingColor, $bags, $containingBags);
+        }
+    }
+}
---
+++ src/Year2020/Day07/PartTwo.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AOC\Year2020\Day07;
+
+use AOC\Util\CachedCallableArray;
+use Generator;
+use Safe\Exceptions\PcreException;
+
+use function Safe\preg_match;
+
+class PartTwo
+{
+    /**
+     * @param Generator<string> $input
+     *
+     * @throws PcreException
+     *
+     * @return int
+     */
+    public function __invoke(Generator $input): int
+    {
+        /** @var CachedCallableArray<string, int> $bags */
+        $bags = new CachedCallableArray();
+
+        foreach ($input as $line) {
+            if (!preg_match('/(?<color>\w+ \w+) bags contain (?<contain>[^.]+)\./', $line, $m)) {
+                continue;
+            }
+
+            $color = $m['color'];
+
+            if ($m['contain'] === 'no other bags') {
+                $bags->set($color, fn() => 0);
+                continue;
+            }
+
+            $containedBags = array_reduce(explode(', ', $m['contain']), function ($carry, $item) {
+                if (!preg_match('/(?<number>\d+) (?<color>\w+ \w+) bags?/', $item, $m)) {
+                    return $carry;
+                }
+
+                $carry[] = ['number' => (int) $m['number'], 'color' => $m['color']];
+                return $carry;
+            }, []);
+
+            $bags->set($color, function () use ($containedBags, $bags) {
+                return array_reduce($containedBags, static function ($carry, $item) use ($bags) {
+                    $carry += $item['number'] * ($bags->get($item['color']) + 1);
+                    return $carry;
+                }, 0);
+            });
+        }
+
+        return $bags->get('shiny gold');
+    }
+}
