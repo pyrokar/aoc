de379c542442bb0d91b4649936e5e6759988f3e0
Gunter Solf <gunter.solf@gmail.com>
2024-12-11 14:53:30 +0100

    2024 11 solution

---
+++ src/Year2024/Day11/Solution.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AOC\Year2024\Day11;
+
+use function array_map;
+use function explode;
+use function strlen;
+use function substr;
+
+final class Solution
+{
+    /**
+     * @var array<int, array<int, int>>
+     */
+    private array $cache;
+
+    /**
+     * @param string $input
+     * @param int $times
+     *
+     * @return int
+     */
+    public function __invoke(string $input, int $times): int
+    {
+        $stones = array_map('intval', explode(' ', $input));
+
+        $this->cache = [];
+
+        $sum = 0;
+
+        foreach ($stones as $stone) {
+            $sum += $this->getStones($stone, $times);
+        }
+
+        return $sum;
+    }
+
+    private function getStones(int $stone, int $times): int
+    {
+        if ($times === 0) {
+            return 1;
+        }
+
+        if (isset($this->cache[$stone][$times])) {
+            return $this->cache[$stone][$times];
+        }
+
+        $countStones = 0;
+
+        if ($stone === 0) {
+            $countStones += $this->getStones(1, $times - 1);
+        } else {
+            $l = strlen((string) $stone);
+            if ($l % 2 === 0) {
+                $countStones += $this->getStones((int) substr((string) $stone, 0, (int) ($l / 2)), $times - 1);
+                $countStones += $this->getStones((int) substr((string) $stone, (int) ($l / 2)), $times - 1);
+            } else {
+                $countStones += $this->getStones($stone * 2024, $times - 1);
+            }
+        }
+
+        $this->cache[$stone][$times] = $countStones;
+
+        return $countStones;
+    }
+}
---
+++ tests/Year2024/Day11/PartTwoTest.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AOCTest\Year2024\Day11;
+
+use AOC\Year2024\Day11\Solution;
+use AOCTest\Util\SolutionTestCase;
+use Safe\Exceptions\FilesystemException;
+
+final class PartTwoTest extends SolutionTestCase
+{
+    /** @var class-string */
+    public string $solutionClass = Solution::class;
+
+
+    /**
+     * @throws FilesystemException
+     *
+     * @return array<int, array<mixed>>
+     */
+    #[\Override]
+    public function data(): array
+    {
+        return [
+            [[$this->lineFromFile(__DIR__ . DS . 'input'), 75], 259593838049805],
+        ];
+    }
+}
